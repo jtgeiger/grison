77686017048a1cb0192677a57491a212484fcd35
Joel Geiger <joel@sibilantsolutions.com>
2014-08-20 14:04:52 -0400

    Split Foscam device search into its own class.

--- src/main/java/com/sibilantsolutions/grison/driver/foscam/net/FoscamService.java
+++ src/main/java/com/sibilantsolutions/grison/driver/foscam/net/FoscamService.java
@@ -1,15 +1,5 @@
 package com.sibilantsolutions.grison.driver.foscam.net;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetSocketAddress;
-import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.sibilantsolutions.grison.driver.foscam.domain.AudioStartReqText;
 import com.sibilantsolutions.grison.driver.foscam.domain.AudioStartRespText;
 import com.sibilantsolutions.grison.driver.foscam.domain.Command;
@@ -17,22 +7,16 @@ import com.sibilantsolutions.grison.driver.foscam.domain.LoginReqText;
 import com.sibilantsolutions.grison.driver.foscam.domain.LoginRespText;
 import com.sibilantsolutions.grison.driver.foscam.domain.OperationProtocolOpCodeE;
 import com.sibilantsolutions.grison.driver.foscam.domain.ProtocolE;
-import com.sibilantsolutions.grison.driver.foscam.domain.SearchProtocolOpCodeE;
-import com.sibilantsolutions.grison.driver.foscam.domain.SearchReqText;
-import com.sibilantsolutions.grison.driver.foscam.domain.SearchRespText;
 import com.sibilantsolutions.grison.driver.foscam.domain.TalkStartReqText;
 import com.sibilantsolutions.grison.driver.foscam.domain.TalkStartRespText;
 import com.sibilantsolutions.grison.driver.foscam.domain.VerifyReqText;
 import com.sibilantsolutions.grison.driver.foscam.domain.VerifyRespText;
 import com.sibilantsolutions.grison.driver.foscam.domain.VideoStartReqText;
 import com.sibilantsolutions.grison.driver.foscam.domain.VideoStartRespText;
-import com.sibilantsolutions.iptools.event.DatagramReceiveEvt;
-import com.sibilantsolutions.iptools.event.DatagramReceiverI;
-import com.sibilantsolutions.iptools.net.SocketUtils;
 
 public class FoscamService
 {
-    final static private Logger log = LoggerFactory.getLogger( FoscamService.class );
+    //final static private Logger log = LoggerFactory.getLogger( FoscamService.class );
 
     private FoscamConnection connection;
 
@@ -75,112 +59,6 @@ public class FoscamService
         login.setDataConnectionId( dataConnectionId );
 
         connection.sendAsync( c );
-    }
-
-    static public List<SearchRespText> deviceSearch( int searchDurationMs )
-    {
-        return deviceSearch( searchDurationMs, 0 );    //Choose a random port.
-    }
-
-    static public List<SearchRespText> deviceSearch( int searchDurationMs, int localPort )
-    {
-        Command c = new Command();
-        c.setProtocol( ProtocolE.SEARCH_PROTOCOL );
-        c.setOpCode( SearchProtocolOpCodeE.Search_Req );
-        SearchReqText text = new SearchReqText();
-        c.setCommandText( text );
-
-        byte[] buf = c.toDatastream();
-
-            //This address:port is defined in the Foscam docs.
-        InetSocketAddress destinationAddress = new InetSocketAddress( "255.255.255.255", 10000 );
-
-        final DatagramPacket packet;
-        try
-        {
-            packet = new DatagramPacket( buf, buf.length, destinationAddress );
-        }
-        catch ( SocketException e )
-        {
-            // TODO Auto-generated catch block
-            throw new UnsupportedOperationException( "MY TODO!", e );
-        }
-
-        final DatagramSocket socket;
-        try
-        {
-            //Make sure that your firewall is open to receive UDP packets on this local port.
-            socket = new DatagramSocket( localPort );
-        }
-        catch ( SocketException e )
-        {
-            // TODO Auto-generated catch block
-            throw new UnsupportedOperationException( "MY TODO!", e );
-        }
-
-        SocketUtils.send( packet, socket );
-
-        final List<SearchRespText> searchResponses = new ArrayList<SearchRespText>();
-
-        DatagramReceiverI receiver = new DatagramReceiverI() {
-
-            @Override
-            public void onReceive( DatagramReceiveEvt evt )
-            {
-                DatagramPacket receivePacket = evt.getPacket();
-
-                Command cReceive = Command.parse( receivePacket.getData(),
-                        receivePacket.getOffset(), receivePacket.getLength() );
-
-                switch ( cReceive.getProtocol() )
-                {
-
-                    case SEARCH_PROTOCOL:
-                        SearchProtocolOpCodeE opCode = (SearchProtocolOpCodeE)cReceive.getOpCode();
-                        switch ( opCode )
-                        {
-                            case Search_Resp:
-                                SearchRespText srt = (SearchRespText)cReceive.getCommandText();
-                                searchResponses.add( srt );
-                                break;
-
-                            default:
-                                throw new IllegalArgumentException( "Unexpected opcode=" + opCode );
-
-                        }
-                        break;
-
-                    default:
-                        throw new IllegalArgumentException( "Unexpected protocol=" + cReceive.getProtocol() );
-                };
-            }
-        };
-
-        byte[] receiveBuf = new byte[4096];
-        DatagramPacket receivePacket = new DatagramPacket( receiveBuf, receiveBuf.length );
-
-        log.info( "Starting to listen on socket={} for search responses.  Make sure UDP port={} is allowed through any firewall.",
-                socket.getLocalSocketAddress(), socket.getLocalPort() );
-
-        SocketUtils.receiveLoopThread( receivePacket, socket, receiver );
-
-        log.info( "Sleeping {}ms to wait for search responses.", searchDurationMs );
-
-        try
-        {
-            Thread.sleep( searchDurationMs );
-        }
-        catch ( InterruptedException e )
-        {
-            // TODO Auto-generated catch block
-            throw new UnsupportedOperationException( "MY TODO!", e );
-        }
-
-        log.info( "Awake after {}ms; done waiting for search responses.", searchDurationMs );
-
-        socket.close();
-
-        return searchResponses;
     }
 
     public LoginRespText login()
