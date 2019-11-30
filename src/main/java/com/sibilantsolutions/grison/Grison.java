3ac8e51e754716a5601d4d4829560b5641d66ee2
Joel Geiger <joel@sibilantsolutions.com>
2019-11-30 15:16:33 -0500

    Install RxJava global error handler.

--- src/main/java/com/sibilantsolutions/grison/Grison.java
+++ src/main/java/com/sibilantsolutions/grison/Grison.java
@@ -1,10 +1,15 @@
 package com.sibilantsolutions.grison;
 
+import java.io.IOException;
+import java.net.SocketException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.sibilantsolutions.grison.demo.ApiDemo;
 import com.sibilantsolutions.grison.demo.Demo;
+import io.reactivex.exceptions.UndeliverableException;
+import io.reactivex.plugins.RxJavaPlugins;
 
 /**
  * Grisons, also known as South American wolverines, are mustelids native to Central and South America.
@@ -18,6 +23,8 @@ public class Grison
     static public void main( final String[] args )
     {
         log.info( "main() started." );
+
+        rxInit();
 
         int i = 0;
         final String host = args[i++];
@@ -37,6 +44,37 @@ public class Grison
 
         log.info("main() finished.");
 
+    }
+
+    private static void rxInit() {
+        RxJavaPlugins.setErrorHandler(e -> {
+            boolean isUndeliverableException = false;
+            if (e instanceof UndeliverableException) {
+                isUndeliverableException = true;
+                e = e.getCause();
+            }
+            if ((e instanceof IOException) || (e instanceof SocketException)) {
+                // fine, irrelevant network problem or API that throws on cancellation
+                return;
+            }
+            if (e instanceof InterruptedException) {
+                // fine, some blocking code was interrupted by a dispose call
+                return;
+            }
+            if ((e instanceof NullPointerException) || (e instanceof IllegalArgumentException)) {
+                // that's likely a bug in the application
+                Thread.currentThread().getUncaughtExceptionHandler()
+                        .uncaughtException(Thread.currentThread(), e);
+                return;
+            }
+            if (e instanceof IllegalStateException) {
+                // that's a bug in RxJava or in a custom operator
+                Thread.currentThread().getUncaughtExceptionHandler()
+                        .uncaughtException(Thread.currentThread(), e);
+                return;
+            }
+            log.warn("Undeliverable exception received, not sure what to do (source was UE={})", isUndeliverableException, e);
+        });
     }
 
 }
